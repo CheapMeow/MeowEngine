ecc302d5d20611c0554b41d6be7c87c462bb8fc8
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2023-09-05 16:12:51 +0800

    NonCopyable class

---
+++ src/runtime/core/base/non_copyable.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "core/base/macro.h"
+
+namespace Meow
+{
+    /**
+     * @brief Class that removes the copy constructor and operator from derived classes, while leaving move.
+     */
+    class LIBRARY_EXPORTS NonCopyable
+    {
+    protected:
+        NonCopyable()          = default;
+        virtual ~NonCopyable() = default;
+
+    public:
+        NonCopyable(const NonCopyable&)                = delete;
+        NonCopyable(NonCopyable&&) noexcept            = default;
+        NonCopyable& operator=(const NonCopyable&)     = delete;
+        NonCopyable& operator=(NonCopyable&&) noexcept = default;
+    };
+} // namespace Meow
