6eddf993021bbaf29fe71dbd5d2cf4ec1b8b42f1
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2024-11-25 16:46:34 +0800

    space

--- src/meow_runtime/function/render/utils/model_utils.cpp
+++ src/meow_runtime/function/render/utils/model_utils.cpp
@@ -46,7 +46,7 @@ namespace Meow
             -1.0f, -1.0f, -1.0f, // top-right
             // top face
             -1.0f,  1.0f, -1.0f, // top-left
-             1.0f,  1.0f , 1.0f, // bottom-right
+             1.0f,  1.0f,  1.0f, // bottom-right
              1.0f,  1.0f, -1.0f, // top-right     
              1.0f,  1.0f,  1.0f, // bottom-right
             -1.0f,  1.0f, -1.0f, // top-left
