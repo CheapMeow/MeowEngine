36a22bb338dfe91a93dde7c853e224eb5e31df5c
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2025-03-12 20:49:52 +0800

    testing build config

--- build_edtior_debug.bat
+++ build_edtior_debug.bat
@@ -4,6 +4,6 @@ cls
 
 REM Configure a debug build
 cmake -S . -B build/ -G "Visual Studio 17 2022" -A x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=EditorDebug
-cmake --build build/ --parallel 8
+cmake --build build/ --parallel 8 --config EditorDebug
 
 pause
