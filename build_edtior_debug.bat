5fb6d714035e1c67ee0c22545dfbb12cfb167d42
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2025-03-14 00:27:35 +0800

    fix bug of configuration type setting

--- build_edtior_debug.bat
+++ build_edtior_debug.bat
@@ -3,7 +3,7 @@
 cls
 
 REM Configure a debug build
-cmake -S . -B build/ -G "Visual Studio 17 2022" -A x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=EditorDebug
-cmake --build build/ --parallel 8 --config EditorDebug
+cmake -S . -B build/ -G "Visual Studio 17 2022" -A x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=EDITORDEBUG
+cmake --build build/ --parallel 8 --config EDITORDEBUG
 
 pause
--- build_edtior_release.bat
+++ build_edtior_release.bat
@@ -3,7 +3,7 @@
 cls
 
 REM Configure a debug build
-cmake -S . -B build/ -G "Visual Studio 17 2022" -A x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=EditorRelease
-cmake --build build/ --parallel 8 --config EditorRelease
+cmake -S . -B build/ -G "Visual Studio 17 2022" -A x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=EDITORRELEASE
+cmake --build build/ --parallel 8 --config EDITORRELEASE
 
 pause
