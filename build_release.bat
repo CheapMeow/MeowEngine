9fc11d29ef9dc38acdc4612dc5a33fb2776554e2
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2026-05-17 21:42:21 +0800

    add windows notify

--- build_release.bat
+++ build_release.bat
@@ -6,4 +6,10 @@ REM Configure a debug build
 cmake -S . -B build/ -G "Visual Studio 17 2022" -A x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release
 cmake --build build/ --parallel 8 --config Release
 
+REM 锁定 UTF-8
+chcp 65001 >nul
+
+REM Windows 气泡提示
+powershell -NoProfile -WindowStyle Hidden -Command "[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $obj=New-Object System.Windows.Forms.NotifyIcon; $obj.Icon=[System.Drawing.Icon]::ExtractAssociatedIcon('C:\Windows\explorer.exe'); $obj.BalloonTipIcon='Info'; $obj.BalloonTipTitle='MeowEngine'; $obj.BalloonTipText='构建完成。'; $obj.Visible=$true; $obj.ShowBalloonTip(10000);"
+
 pause
